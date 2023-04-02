4bd8ed108b3b1d4f2cf4e5ac20f69b07b3bf332e
simon.hardt@collyphery.com <s-Q-6qJn@XQi!RBXq@.A>
2023-04-02 20:44:10 +0200

    Added, Finish line and level up

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import random
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from scoreboard import Scoreboard
 from player import Player
 from car_manager import CarManager
+
+CAR_SPEED = 0.1
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -26,6 +27,15 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+        if player.is_at_finish_line():
+            player.go_to_start()
+            car_manager.level_up()
 
 
 screen.exitonclick()
--- player.py
+++ player.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINIS_LINE_Y = 280
+FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
@@ -12,10 +12,19 @@ class Player(Turtle):
         self.color("black")
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def up(self):
         self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
 
 
