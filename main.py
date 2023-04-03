ccd7214e013e24028cc426e7a20a919d85accc01
simon.hardt@collyphery.com <s-Q-6qJn@XQi!RBXq@.A>
2023-04-03 19:46:31 +0200

    Added: Increase score, GameOver

--- main.py
+++ main.py
@@ -32,10 +32,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
-        if player.is_at_finish_line():
-            player.go_to_start()
-            car_manager.level_up()
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,18 +1,27 @@
 from turtle import Turtle
 
+FONT = ("Courier", 24, "normal")
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.color("black")
-        self.penup()
         self.hideturtle()
-        self.score = 0
+        self.penup()
+        self.color("black")
+        self.score = 1
+        self.goto(0, 260)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(0, 260)
         self.write(self.score, align="center", font=("Courier", 40, "normal"))
 
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="left", font=FONT)
